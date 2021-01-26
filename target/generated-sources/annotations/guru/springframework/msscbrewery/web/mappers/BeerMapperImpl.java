52b2f66b94dbaa3c069c595dbdceaf81e789cdf1
Nathan Lehman <nlehman@dlcouch.com>
2021-01-26 10:26:03 -0500

    mapstruct

---
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
@@ -0,0 +1,57 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import guru.springframework.msscbrewery.domain.Beer;
+import guru.springframework.msscbrewery.domain.Beer.BeerBuilder;
+import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2;
+import guru.springframework.msscbrewery.web.model.v2.BeerDtoV2.BeerDtoV2Builder;
+import javax.annotation.processing.Generated;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-01-26T10:22:21-0500",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 15.0.1 (Oracle Corporation)"
+)
+@Component
+public class BeerMapperImpl implements BeerMapper {
+
+    @Autowired
+    private DateMapper dateMapper;
+
+    @Override
+    public BeerDtoV2 beerToBeerDto(Beer beer) {
+        if ( beer == null ) {
+            return null;
+        }
+
+        BeerDtoV2Builder beerDtoV2 = BeerDtoV2.builder();
+
+        beerDtoV2.id( beer.getId() );
+        beerDtoV2.beerName( beer.getBeerName() );
+        beerDtoV2.beerStyle( beer.getBeerStyle() );
+        beerDtoV2.upc( beer.getUpc() );
+        beerDtoV2.createdDate( dateMapper.asOffsetDateTime( beer.getCreatedDate() ) );
+        beerDtoV2.lastUpdatedDate( dateMapper.asOffsetDateTime( beer.getLastUpdatedDate() ) );
+
+        return beerDtoV2.build();
+    }
+
+    @Override
+    public Beer beerDtoToBeer(BeerDtoV2 dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        BeerBuilder beer = Beer.builder();
+
+        beer.id( dto.getId() );
+        beer.beerName( dto.getBeerName() );
+        beer.beerStyle( dto.getBeerStyle() );
+        beer.upc( dto.getUpc() );
+        beer.createdDate( dateMapper.asTimestamp( dto.getCreatedDate() ) );
+        beer.lastUpdatedDate( dateMapper.asTimestamp( dto.getLastUpdatedDate() ) );
+
+        return beer.build();
+    }
+}
---
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/CustomerMapperImpl.java
@@ -0,0 +1,45 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import guru.springframework.msscbrewery.domain.Customer;
+import guru.springframework.msscbrewery.domain.Customer.CustomerBuilder;
+import guru.springframework.msscbrewery.web.model.CustomerDto;
+import guru.springframework.msscbrewery.web.model.CustomerDto.CustomerDtoBuilder;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-01-26T10:22:21-0500",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 15.0.1 (Oracle Corporation)"
+)
+@Component
+public class CustomerMapperImpl implements CustomerMapper {
+
+    @Override
+    public CustomerDto customerToCustomerDto(Customer customer) {
+        if ( customer == null ) {
+            return null;
+        }
+
+        CustomerDtoBuilder customerDto = CustomerDto.builder();
+
+        customerDto.id( customer.getId() );
+        customerDto.name( customer.getName() );
+
+        return customerDto.build();
+    }
+
+    @Override
+    public Customer customerDtoToCustomer(CustomerDto dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        CustomerBuilder customer = Customer.builder();
+
+        customer.id( dto.getId() );
+        customer.name( dto.getName() );
+
+        return customer.build();
+    }
+}

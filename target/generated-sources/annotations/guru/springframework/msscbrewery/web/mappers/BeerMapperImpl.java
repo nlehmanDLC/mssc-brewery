c464bb4fe66c8582f5f8f5c989dc09586a3f784d
Nathan Lehman <nlehman@dlcouch.com>
2021-01-26 10:38:10 -0500

    adding mapstruct. closes #10

--- target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/msscbrewery/web/mappers/BeerMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-01-26T10:22:21-0500",
+    date = "2021-01-26T10:36:09-0500",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 15.0.1 (Oracle Corporation)"
 )
 @Component
